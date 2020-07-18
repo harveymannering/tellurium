4d95f307e30d83b07dc534a0cb4ad069862080b8
Ciaran Welsh <cwelsh2@uw.edu>
2020-07-18 10:58:32 +0100

    test_temiriam.py now uses the unittest package. It has a test class derived from unittest.TestCase but the tests are not particularly comprehensive.

--- tellurium/tests/test_temiriam.py
+++ tellurium/tests/test_temiriam.py
@@ -6,32 +6,34 @@ from six import string_types
 import roadrunner
 from tellurium import temiriam
 
+import unittest
 
-def test_getSBMLFromBiomodelsURN1():
-    """ Check that string is returned.
+class TeMiriamTests(unittest.TestCase):
 
-    :return:
-    """
-    urn = 'urn:miriam:biomodels.db:BIOMD0000000139'
-    sbml = temiriam.getSBMLFromBiomodelsURN(urn)
-    assert sbml is not None
-    # check that string
-    assert isinstance(sbml, string_types)
+    def test_getSBMLFromBiomodelsURN1(self):
+        """ Check that string is returned.
 
+        :return:
+        """
+        urn = 'urn:miriam:biomodels.db:BIOMD0000000139'
+        sbml = temiriam.getSBMLFromBiomodelsURN(urn)
+        assert sbml is not None
+        # check that string
+        self.assertIsInstance(sbml, string_types)
 
-def test_getSBMLFromBiomodelsURN1():
-    """ Check that model can be loaded in roadrunner.
+    def test_getSBMLFromBiomodelsURN2(self):
+        """ Check that model can be loaded in roadrunner.
 
-    :return:
-    """
-    urn = 'urn:miriam:biomodels.db:BIOMD0000000139'
-    sbml = temiriam.getSBMLFromBiomodelsURN(urn)
+        :return:
+        """
+        urn = 'urn:miriam:biomodels.db:BIOMD0000000139'
+        sbml = temiriam.getSBMLFromBiomodelsURN(urn)
 
-    print("*" * 80)
-    print(type(sbml))
-    print("*" * 80)
-    print(sbml)
-    print("*" * 80)
+        print("*" * 80)
+        print(type(sbml))
+        print("*" * 80)
+        print(sbml)
+        print("*" * 80)
 
-    r = roadrunner.RoadRunner(sbml)
-    assert r is not None
+        r = roadrunner.RoadRunner(sbml)
+        self.assertFalse(r is None)
